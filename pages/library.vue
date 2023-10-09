d81f4892a129287b81d6e684704c131b10a627a8
jesseln <48170789+jesseln@users.noreply.github.com>
2023-10-10 10:10:13 +1100

    delploy fix attempt 32

---
+++ pages/library.vue
@@ -0,0 +1,19 @@
+<template>
+    <div >
+
+
+    <NuxtPage :setQuery="agent01" />
+
+    </div>
+   
+    
+  </template>
+  
+  <script setup>
+
+  </script>
+  
+  <style scoped>
+  
+
+  </style>
